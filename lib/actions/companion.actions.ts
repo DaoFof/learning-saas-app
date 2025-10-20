6fad10cac0c6e4dab9e1997f0d2887d7ff5b3686
DaoFof <daofof0@gmail.com>
2025-10-20 17:44:03 +0000

    Implement companion libray and filtering features

--- lib/actions/companion.actions.ts
+++ lib/actions/companion.actions.ts
@@ -3,6 +3,7 @@
 import { auth } from "@clerk/nextjs/server"
 import { createSupabaseClient } from "../supabase"
 
+// This function is a mutation so we need to check the user here
 export const createCompanion = async (formData: CreateCompanion) => {
     const { userId : author } = await auth()
     const supabase = createSupabaseClient()
@@ -17,4 +18,33 @@ export const createCompanion = async (formData: CreateCompanion) => {
     }
 
     return data[0]
+}
+
+//This function is an action so we do not need to check the user here
+export const getAllCompanions = async ({limit = 10, page = 1, subject, topic} : GetAllCompanions) => {
+    const supabase = createSupabaseClient()
+
+    let query = supabase.from("companions").select()
+
+    if (subject && topic) {
+        query = query
+            .ilike("subject", `%${subject}%`)
+            .or(`topic.ilike.%${topic}%,name.ilike.%${topic}%`)
+    } else if (subject) {
+        query = query.ilike("subject", `%${subject}%`)
+    } else if (topic) {
+        query = query.or(`topic.ilike.%${topic}%,name.ilike.%${topic}%`)
+    }
+
+    query = query.range((page - 1) * limit, page * limit - 1)
+
+    const { data : companions, error } = await query
+
+    if (error) {
+        throw new Error(error.message)
+    }
+
+
+    return companions
+
 }
